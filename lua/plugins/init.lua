7fd99af7c91b41fd79112ce3163fdd181987db40
John Liu <john.liu@xperi.com>
2024-06-05 19:40:37 +0800

    bbq config update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -557,7 +557,6 @@ return {
     "utilyre/barbecue.nvim",
     event = "LspAttach",
     name = "barbecue",
-    version = "*",
     dependencies = {
       "SmiteshP/nvim-navic",
       "nvim-tree/nvim-web-devicons", -- optional dependency
