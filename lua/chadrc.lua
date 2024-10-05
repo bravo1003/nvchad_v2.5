bb3383b8a15aedad1816bafaba1f4c3a74f212f0
Bravo1003 <bravo1003@gmail.com>
2024-10-05 19:43:21 +0800

    Update to conform with nvchad update

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -3,84 +3,101 @@
 -- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
-local M = {}
 
 -- Path to overriding theme and highlights files
 local highlights = require "highlights"
 
-M.base46 = {
-  theme = "tokyonight", -- default theme
-  theme_toggle = { "tokyonight", "monekai" },
-  changed_themes = {
-    monekai = require "theme.monokai-pro",
-  },
+local options = {
+  base46 = {
+    theme = "tokyonight", -- default theme
+    theme_toggle = { "tokyonight", "monekai" },
+    changed_themes = {
+      monekai = require "theme.monokai-pro",
+    },
 
-  hl_add = highlights.add,
-  hl_override = highlights.override,
-  transparency = false,
+    hl_add = highlights.add,
+    hl_override = highlights.override,
+    transparency = false,
 
-  integrations = {
-    "notify",
-    "rainbowdelimiters",
-    "hop",
-    "todo",
-    "trouble",
-  },
-}
-
-M.ui = {
-  cmp = {
-    icons = true,
-    lspkind_text = true,
-    style = "default",
-    selected_item_bg = "colored",
-  },
-
-  statusline = {
-    theme = "default",
-    separator_style = "default",
-    modules = {
-      lsp = function()
-        return "%#St_Lsp#" .. require("utils.lsp-statusline").lsp_overriden()
-      end,
+    integrations = {
+      "notify",
+      "rainbowdelimiters",
+      "hop",
+      "todo",
+      "trouble",
     },
   },
 
-  -- lazyload it when there are 1+ buffers
-  tabufline = {
-    show_numbers = false,
-    enabled = true,
-    lazyload = true,
-    order = {
-      -- if disable nvimtree float, uncomment to add tree offset
-      -- "treeOffset"
-      "buffers",
-      "tabs",
-      "btns",
+  ui = {
+    cmp = {
+      icons = true,
+      lspkind_text = true,
+      style = "default",
+      selected_item_bg = "colored",
+    },
+
+    statusline = {
+      theme = "default",
+      separator_style = "default",
+      modules = {
+        lsp = function()
+          return "%#St_Lsp#" .. require("utils.lsp-statusline").lsp_overriden()
+        end,
+      },
+    },
+
+    -- lazyload it when there are 1+ buffers
+    tabufline = {
+      show_numbers = false,
+      enabled = true,
+      lazyload = true,
+      order = {
+        -- if disable nvimtree float, uncomment to add tree offset
+        -- "treeOffset"
+        "buffers",
+        "tabs",
+        "btns",
+      },
     },
   },
 
   nvdash = {
     load_on_startup = true,
     header = {
-      "           ▄ ▄                   ",
-      "       ▄   ▄▄▄     ▄ ▄▄▄ ▄ ▄     ",
-      "       █ ▄ █▄█ ▄▄▄ █ █▄█ █ █     ",
-      "    ▄▄ █▄█▄▄▄█ █▄█▄█▄▄█▄▄█ █     ",
-      "  ▄ █▄▄█ ▄ ▄▄ ▄█ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄  ",
-      "  █▄▄▄▄ ▄▄▄ █ ▄ ▄▄▄ ▄ ▄▄▄ ▄ ▄ █ ▄",
-      "▄ █ █▄█ █▄█ █ █ █▄█ █ █▄█ ▄▄▄ █ █",
-      "█▄█ ▄ █▄▄█▄▄█ █ ▄▄█ █ ▄ █ █▄█▄█ █",
-      "    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█ █▄█▄▄▄█    ",
+      "                            ",
+      "     ▄▄         ▄ ▄▄▄▄▄▄▄   ",
+      "   ▄▀███▄     ▄██ █████▀    ",
+      "   ██▄▀███▄   ███           ",
+      "   ███  ▀███▄ ███           ",
+      "   ███    ▀██ ███           ",
+      "   ███      ▀ ███           ",
+      "   ▀██ █████▄▀█▀▄██████▄    ",
+      "     ▀ ▀▀▀▀▀▀▀ ▀▀▀▀▀▀▀▀▀▀   ",
+      "                            ",
+      "     Powered By  eovim    ",
+      "                            ",
     },
 
     buttons = {
-      { "  Find File", "Spc s f", "Telescope find_files" },
-      { "󰈚  Recent Files", "Spc s o", "Telescope oldfiles" },
-      { "󰈭  Find Word", "Spc s g", "Telescope live_grep" },
-      { "  Bookmarks", "Spc m a", "Telescope marks" },
-      { "  Themes", "Spc n t", "Telescope themes" },
-      { "  Mappings", "Spc n c", "NvCheatsheet" },
+      { txt = "  Find File", keys = "Spc s f", cmd = "Telescope find_files theme=ivy" },
+      { txt = "  Recent Files", keys = "Spc s o", cmd = "Telescope oldfiles theme=ivy" },
+      { txt = "󰈭  Find Word", keys = "Spc s g", cmd = "Telescope live_grep theme=ivy" },
+      { txt = "󱥚  Themes", keys = "Spc t h", cmd = ":lua require('nvchad.themes').open()" },
+      { txt = "  Mappings", keys = "Spc c h", cmd = "NvCheatsheet" },
+
+      { txt = "─", hl = "NvDashLazy", no_gap = true, rep = true },
+
+      {
+        txt = function()
+          local stats = require("lazy").stats()
+          local ms = math.floor(stats.startuptime) .. " ms"
+          return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
+        end,
+        hl = "NvDashLazy",
+        no_gap = true,
+      },
+
+      { txt = "─", hl = "NvDashLazy", no_gap = true, rep = true },
     },
   },
 
@@ -97,12 +114,30 @@ M.ui = {
       -- lua stuff
       "lua-language-server",
       "stylua",
+      "luacheck",
+
+      -- web dev stuff
+      -- "css-lsp",
+      -- "html-lsp",
+      -- "typescript-language-server",
+      -- "deno",
       "prettier",
+      "yaml-language-server",
+      "yamllint",
 
       -- c/cpp stuff
       "cpplint",
       "clangd",
       "clang-format",
+      "codelldb",
+
+      -- rust
+      "rust-analyzer",
+
+      -- go
+      -- "delve",
+      -- "gopls",
+      -- "golangci-lint",
 
       -- markdown
       "markdownlint",
@@ -111,6 +146,12 @@ M.ui = {
       "shfmt",
     },
   },
+
+  colorify = {
+    mode = "bg", -- fg, bg, virtual
+    virt_text = "󱓻 ",
+    highlight = { hex = true, lspvars = true },
+  },
 }
 
-return M
+return options
--- lua/utils/lsputils.lua
+++ lua/utils/lsputils.lua
@@ -1,18 +1,15 @@
 local M = {}
 
-local conf = require("nvconfig").ui.lsp
+local conf = require "nvconfig"
 
 -- disable semantic tokens
 M.on_init = function(client, _)
-  if not conf.semantic_tokens and client.supports_method "textDocument/semanticTokens" then
+  if not conf.lsp.semantic_tokens and client.supports_method "textDocument/semanticTokens" then
     client.server_capabilities.semanticTokensProvider = nil
   end
 end
 
 M.on_attach = function(client, bufnr)
-  if conf.signature and client.server_capabilities.signatureHelpProvider then
-    require("nvchad.lsp.signature").setup(client, bufnr)
-  end
   require("utils.lspkeyremaps").keymaps()
 end
 
