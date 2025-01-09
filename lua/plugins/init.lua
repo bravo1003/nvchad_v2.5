3e533faaf27b3b6e877f609a7cac721da44963cd
John Liu <john.liu@xperi.com>
2025-01-09 18:20:08 +0800

    Add esc key for entering normal mode for toggle term

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -467,6 +467,12 @@ return {
         end,
         on_close = function(_) end,
       }
+      function _G.set_terminal_keymaps()
+        local opts = { noremap = true }
+        vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
+      end
+
+      vim.cmd "autocmd! TermOpen term://* lua set_terminal_keymaps()"
     end,
   },
 
