92b13903274b51f8216bd90e00bd3a51b9422bf0
John Liu <john.liu@xperi.com>
2025-07-17 12:08:13 +0800

    remove uneeded plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -355,10 +355,7 @@ return {
     ft = { "markdown", "Avante" },
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
-      "echasnovski/mini.nvim",
     }, -- if you use the mini.nvim suite
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
     opts = {
       file_types = { "markdown", "Avante" },
       completions = {
