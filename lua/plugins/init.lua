cc53af06a72e1263d281db3992644c2ec4b2709d
John Liu <john.liu@xperi.com>
2025-07-21 14:06:56 +0800

    Add ":" trigger for c++ in blink.cmp

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -170,7 +170,7 @@ return {
           selection = { preselect = false, auto_insert = true },
         },
         trigger = {
-          show_on_x_blocked_trigger_characters = { "<", "'", '"', "(" },
+          show_on_x_blocked_trigger_characters = { "<", ":", "'", '"', "(" },
         },
       },
     },
