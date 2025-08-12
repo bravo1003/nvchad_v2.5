09f477b70e0d3c3a60ccb018d0b2efd7a0e0c04e
Bravo1003 <bravo1003@gmail.com>
2025-08-13 00:02:10 +0800

    disable uneeded autocommands

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,13 +1,3 @@
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  desc = "Change c/c++ comment to linewise",
-  -- group = ...,
-  callback = function()
-    if vim.tbl_contains({ "c", "cpp" }, vim.bo.filetype) then
-      vim.bo.commentstring = "// %s"
-    end
-  end,
-})
-
 -- Highlight on yank
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
@@ -62,47 +52,47 @@ vim.api.nvim_create_autocmd("BufEnter", {
 })
 
 -- Focus nvim ignored files
-local ignore_filetypes = {
-  "Avante",
-  "AvanteSelectedFiles",
-  "AvanteTodos",
-  "AvanteInput",
-  "NvimTree",
-  "undotree",
-  "diff",
-  "lazy",
-  "toggleterm",
-  "dapui_scopes",
-  "dapui_breakpoints",
-  "dapui_stacks",
-  "dapui_watchtes",
-  "dapui_console",
-  "dap-repl",
-  "trouble",
-  "fugitiveblame",
-}
-local ignore_buftypes = { "nowrite", "nofile", "prompt", "popup", "terminal", "quickfix" }
-local augroup_focus = vim.api.nvim_create_augroup("FocusDisable", { clear = true })
-vim.api.nvim_create_autocmd("WinEnter", {
-  group = augroup_focus,
-  callback = function(_)
-    if vim.tbl_contains(ignore_buftypes, vim.bo.buftype) then
-      vim.w.focus_disable = true
-    else
-      vim.w.focus_disable = false
-    end
-  end,
-  desc = "Disable focus autoresize for BufType",
-})
-
-vim.api.nvim_create_autocmd("FileType", {
-  group = augroup_focus,
-  callback = function(_)
-    if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
-      vim.w.focus_disable = true
-    else
-      vim.w.focus_disable = false
-    end
-  end,
-  desc = "Disable focus autoresize for FileType",
-})
+-- local ignore_filetypes = {
+--   "Avante",
+--   "AvanteSelectedFiles",
+--   "AvanteTodos",
+--   "AvanteInput",
+--   "NvimTree",
+--   "undotree",
+--   "diff",
+--   "lazy",
+--   "toggleterm",
+--   "dapui_scopes",
+--   "dapui_breakpoints",
+--   "dapui_stacks",
+--   "dapui_watchtes",
+--   "dapui_console",
+--   "dap-repl",
+--   "trouble",
+--   "fugitiveblame",
+-- }
+-- local ignore_buftypes = { "nowrite", "nofile", "prompt", "popup", "terminal", "quickfix" }
+-- local augroup_focus = vim.api.nvim_create_augroup("FocusDisable", { clear = true })
+-- vim.api.nvim_create_autocmd("WinEnter", {
+--   group = augroup_focus,
+--   callback = function(_)
+--     if vim.tbl_contains(ignore_buftypes, vim.bo.buftype) then
+--       vim.w.focus_disable = true
+--     else
+--       vim.w.focus_disable = false
+--     end
+--   end,
+--   desc = "Disable focus autoresize for BufType",
+-- })
+--
+-- vim.api.nvim_create_autocmd("FileType", {
+--   group = augroup_focus,
+--   callback = function(_)
+--     if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+--       vim.w.focus_disable = true
+--     else
+--       vim.w.focus_disable = false
+--     end
+--   end,
+--   desc = "Disable focus autoresize for FileType",
+-- })
