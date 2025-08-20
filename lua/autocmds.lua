4e5137e733579c6db3e5eaa7cee6ee920adccec3
John Liu <john.liu@xperi.com>
2025-08-20 17:32:53 +0800

    remove markdown spellcheck

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -21,7 +21,7 @@ vim.api.nvim_create_autocmd("BufRead", {
 })
 
 --  Markdown, commit  spells checks
-local spell_check_filetypes = { "markdown", "gitcommit" }
+local spell_check_filetypes = { "gitcommit" }
 local augroup = vim.api.nvim_create_augroup("SpellCheckEnable", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
   group = augroup,
