1e54c17a7189c4f3e92733bb14491417998818a6
John Liu <john.liu@xperi.com>
2025-08-27 13:43:37 +0800

    lazygit set size 0 for fullscreen

--- lua/configs/overrides.lua
+++ lua/configs/overrides.lua
@@ -243,8 +243,8 @@ M.snacks = {
   },
   styles = {
     lazygit = {
-      width = vim.opt.columns:get(),
-      height = vim.opt.lines:get(),
+      width = 0,
+      height = 0,
     },
     terminal = {
       position = "float",
